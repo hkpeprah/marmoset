2ff0452693f65d3effdda71c7058826d67a1b586
Ford <ford.peprah@uwaterloo.ca>
2014-04-09 23:30:57 -0400

    Fixed broken package structure.

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import marmoset
 
 try:
     from setuptools import setup
@@ -10,15 +9,15 @@ except ImportError:
 
 setup(
     name="Marmoset",
-    version=marmoset.__version__,
-    author=marmoset.__author__,
+    version="1.0.0",
+    author="Ford Peprah",
     author_email="user@example.com",
     packages=['marmoset', 'marmoset.core', 'marmoset.utils'],
     scripts=['bin/marmoset'],
     url="http://example.com",
     license='LICENSE.txt',
     description="Command-line interface for the UofW Marmoset Submission Server",
-    long_description=marmoset.__description__,
+    long_description="",
     install_requires=[
         "beautifulsoup4 >= 4.2.1",
         "mechanize == 0.2.5",
